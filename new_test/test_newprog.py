a15d7db887a02826594028e8fc6ab93f63037b64
JohnDavid <johndavid@simpli.fi>
2017-05-10 15:42:12 -0500

    Tracepoint Modification

--- new_test/test_newprog.py
+++ new_test/test_newprog.py
@@ -3,6 +3,8 @@
 import newprog
 
 def test_func():
+	import ipdb; ipdb.set_trace()
 	variable1 = 1
 	variable2 = 0
+	import ipdb; ipdb.set_trace()
 	assert newprog.func(variable1, variable2) == "True"
